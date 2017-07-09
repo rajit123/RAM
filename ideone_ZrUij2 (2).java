3d56115a5a5ba9f516fbd72f7f3d2ef9526380c9
rajit123 <rajit_deoshankar@srmuniv.edu.in>
2017-07-09 15:00:34 +0530

    Add files via upload

---
+++ ideone_ZrUij2 (2).java
@@ -0,0 +1,10 @@
+public class Vowels
+{
+    public static final char[] CONSONANTS =
+    {
+        'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'
+    };
+
+    public static final char SPACE = ' ';
+
+    public static char[] getConsonants()
